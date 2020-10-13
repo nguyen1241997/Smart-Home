9650a027a9a1e8f81fa09b20d48c4084e3e8bdab
Nguyen Vu Nguyen <nguyen1241997@gmail.com>
2020-10-13 11:21:46 +0700

    water_sensor

--- scr/main.c
+++ scr/main.c
@@ -4,41 +4,42 @@
 #include "stm32f4xx_exti.h"
 
 void exti0_init(void);
+
 void timer_init(void);
+void delay_10_us(uint32_t time);
+
 void request_dht11(void);
 void response_dht11(void);
 uint8_t receive_dht11(void);
+void readDHT11(void);
+	
+void adc_init(void);
+uint16_t ADC_Read(void);
 
 volatile long int count_1=0, count_0=0;
-void delay_ms(uint32_t time);
-volatile int data[100];
 
-GPIO_InitTypeDef DHT11_TypeDef;
+GPIO_InitTypeDef DHT11_TypeDef; //DHT11
 
-uint8_t c=0,I_RH,D_RH,I_Temp,D_Temp,CheckSum;
+uint8_t c, I_RH, D_RH, I_Temp, D_Temp, CheckSum;
+
+uint16_t adcValue;
+char check;
 
 int main(void)
-{
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
-	
+{	
 	timer_init();
+	adc_init();
+	
 	while(1)
 	{
-		request_dht11();
-		response_dht11();
-		I_RH=receive_dht11();	/* store first eight bit in I_RH */
-		D_RH=receive_dht11();	/* store next eight bit in D_RH */
-		I_Temp=receive_dht11();	/* store next eight bit in I_Temp */
-		D_Temp=receive_dht11();	/* store next eight bit in D_Temp */
-		CheckSum=receive_dht11();
-		if ((I_RH + D_RH + I_Temp + D_Temp) == CheckSum)
-			count_1++;
-		delay_ms(100000);
-		count_0++;
+		readDHT11();
+		adcValue = ADC_Read();
+		check = GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_8);
+		delay_10_us(200000);
 	}
 }
 
-void delay_ms(uint32_t time)
+void delay_10_us(uint32_t time)
 {
 	for(int i=0; i<time; i++)
 	{
@@ -120,10 +121,10 @@ void EXTI0_IRQHandler(void)
 {
 	if(EXTI_GetITStatus(EXTI_Line0)!=RESET)
 	{
-		delay_ms(26);
+		delay_10_us(26);
 		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0))
 		{
-			count_0++;
+			
 		}
 		EXTI_ClearITPendingBit(EXTI_Line0);
 	}
@@ -133,10 +134,10 @@ void EXTI1_IRQHandler(void)
 {
 	if(EXTI_GetITStatus(EXTI_Line1)!=RESET)
 	{
-		delay_ms(26);
+		delay_10_us(26);
 		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_1))
 		{
-			count_1++;
+			
 		}
 		EXTI_ClearITPendingBit(EXTI_Line1);
 	}
@@ -144,6 +145,8 @@ void EXTI1_IRQHandler(void)
 
 void request_dht11(void)
 {
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	
 	DHT11_TypeDef.GPIO_Mode = GPIO_Mode_OUT;
 	DHT11_TypeDef.GPIO_OType = GPIO_OType_PP;
 	DHT11_TypeDef.GPIO_PuPd = GPIO_PuPd_NOPULL;
@@ -153,13 +156,15 @@ void request_dht11(void)
 	
 	GPIO_ResetBits(GPIOA, GPIO_Pin_6);
 	
-	delay_ms(2000);
+	delay_10_us(2000);
 	
 	GPIO_SetBits(GPIOA, GPIO_Pin_6);
 }
 
 void response_dht11(void)
 {
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	
 	DHT11_TypeDef.GPIO_Mode = GPIO_Mode_IN;
 	DHT11_TypeDef.GPIO_OType = GPIO_OType_PP;
 	DHT11_TypeDef.GPIO_PuPd = GPIO_PuPd_NOPULL;
@@ -176,7 +181,7 @@ uint8_t receive_dht11(void)
 	for (int q=0; q<8; q++)
 	{
 		while(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_6)==0);
-		delay_ms(5);
+		delay_10_us(5);
 		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_6))/* if high pulse is greater than 30us */
 		{
 			c = (c<<1)|(0x01);
@@ -188,4 +193,64 @@ uint8_t receive_dht11(void)
 		while(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_6));
 	}
 	return c;
+}
+void readDHT11(void)
+{
+		request_dht11();
+		response_dht11();
+		I_RH=receive_dht11();	/* store first eight bit in I_RH */
+		D_RH=receive_dht11();	/* store next eight bit in D_RH */
+		I_Temp=receive_dht11();	/* store next eight bit in I_Temp */
+		D_Temp=receive_dht11();	/* store next eight bit in D_Temp */
+		CheckSum=receive_dht11();
+		
+		if ((I_RH + D_RH + I_Temp + D_Temp) == CheckSum)
+		{
+			
+		}
+}
+void adc_init(void)
+{
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	
+	GPIO_InitTypeDef myLED;
+	myLED.GPIO_Mode = GPIO_Mode_AN;
+	myLED.GPIO_OType = GPIO_OType_PP;
+	myLED.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	myLED.GPIO_Speed = GPIO_Low_Speed;
+	myLED.GPIO_Pin = GPIO_Pin_7;
+	GPIO_Init(GPIOA,&myLED);
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	
+	ADC_InitTypeDef myADC;
+	myADC.ADC_ContinuousConvMode = DISABLE;
+	myADC.ADC_DataAlign = ADC_DataAlign_Right;
+	myADC.ADC_Resolution = ADC_Resolution_12b;
+	myADC.ADC_NbrOfConversion = 1;
+	ADC_Init(ADC1,&myADC);	
+	
+	ADC_RegularChannelConfig (ADC1,ADC_Channel_7,1,ADC_SampleTime_144Cycles);
+	ADC_ITConfig(ADC1,ADC_IT_EOC,ENABLE);
+	ADC_Cmd(ADC1,ENABLE);
+
+}
+uint16_t ADC_Read(void)
+{
+    ADC_SoftwareStartConv(ADC1);
+
+    while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));
+
+    return ADC_GetConversionValue(ADC1);
+}
+void water_sensor_digital_init(void)
+{
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	
+	DHT11_TypeDef.GPIO_Mode = GPIO_Mode_IN;
+	DHT11_TypeDef.GPIO_OType = GPIO_OType_PP;
+	DHT11_TypeDef.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	DHT11_TypeDef.GPIO_Speed = GPIO_High_Speed;
+	DHT11_TypeDef.GPIO_Pin = GPIO_Pin_8;
+	GPIO_Init(GPIOA,&DHT11_TypeDef);
 }
